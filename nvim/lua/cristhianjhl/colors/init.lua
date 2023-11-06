84c8f657fb2e6636baff2dc7fe142e4138109096
cristhianjhlcom <cristhianjhl.web@gmail.com>
2023-11-06 08:30:21 -0500

    update monakai and colors settigs files

--- nvim/lua/cristhianjhl/colors/init.lua
+++ nvim/lua/cristhianjhl/colors/init.lua
@@ -1,24 +1,21 @@
-local theme = 'monakai'
-
 function ColorMyPencils(color)
-  color = color or 'catppuccin'
-  vim.cmd.colorscheme(color)
+    color = color or 'monakai'
+    local ok, color_file = pcall(require, 'cristhianjhl.colors.'..color)
 
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    if not ok then
+      print('Not setup file for color '..color)
+      return
+    end
+
+    if type(color_file.setup) ~= 'function' then
+     print('method setup non existing')
+     return
+    end
+
+    color_file.setup()
+
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 
-local ok, color_file = pcall(require, 'cristhianjhl.colors.'..theme)
-
-if not ok then
-  print('Not setup file for color '..theme)
-end
-
-if type(color_file.setup) ~= 'function' then
- print('method setup non existing')
-end
-
-color_file.setup()
-
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+ColorMyPencils()
--- nvim/lua/cristhianjhl/colors/monakai.lua
+++ nvim/lua/cristhianjhl/colors/monakai.lua
@@ -39,20 +39,22 @@ local function setup()
     monokai.setup({
       palette = palette,
       italics = false,
-      TSInclude = {
-          fg = palette.aqua,
-      },
-      GitSignsAdd = {
-          fg = palette.green,
-          bg = palette.base2
-      },
-      GitSignsDelete = {
-          fg = palette.pink,
-          bg = palette.base2
-      },
-      GitSignsChange = {
-          fg = palette.orange,
-          bg = palette.base2
+      custom_hlgroups = {
+          TSInclude = {
+              fg = palette.aqua,
+          },
+          GitSignsAdd = {
+              fg = palette.green,
+              bg = palette.base2
+          },
+          GitSignsDelete = {
+              fg = palette.pink,
+              bg = palette.base2
+          },
+          GitSignsChange = {
+              fg = palette.orange,
+              bg = palette.base2
+          },
       },
     })
     vim.cmd.colorscheme('monokai')
