eace440b7f3254e0472022815685bc133d671559
Cristhian Hernandez <cristhianjhl.web@gmail.com>
2024-10-05 16:09:08 -0500

    add gitsigns keymaps

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -34,6 +34,14 @@ return {
     vim.keymap.set('n', 'gsb', ":Gitsigns stage_buffer<CR>")
     vim.keymap.set('n', 'gb', ":Gitsigns blame<CR>")
     vim.keymap.set('n', '<Leader>gcc', ":G commit -m ")
-    vim.keymap.set('n', 'gpm', ":G push origin main<CR>")
+        map('n', '<Leader>hs', gitsigns.stage_hunk)
+        map('n', '<Leader>hu', gitsigns.undo_stage_hunk)
+        map('n', '<Leader>hr', gitsigns.reset_hunk)
+        map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('n', '<leader>hS', gitsigns.stage_buffer)
+        map('n', '<leader>hp', gitsigns.preview_hunk)
+        map('n', '<leader>hd', gitsigns.diffthis)
+        map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
   end,
 }
