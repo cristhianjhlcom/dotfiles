bbf73851ad3fe8fe17c136e6ab040ecae1466b5b
Cristhian Hernandez <cristhianjhl.web@gmail.com>
2024-10-05 15:28:41 -0500

    resolvin pull conflicts

---
+++ nvim/lua/config/settings.lua
@@ -0,0 +1,14 @@
+vim.g.netrw_banner = 0
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.showmode = true
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
