665f4783fa7a22a8c61848d1f2c798f8f72a36cf
Cristhian Hernandez <cristhianjhl.web@gmail.com>
2024-10-05 18:12:28 -0500

    add blade lsp server and another servers

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -7,8 +7,13 @@ vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
-vim.opt.showmode = true
+vim.opt.showmode = false
 vim.opt.wrap = false
 vim.opt.signcolumn = "yes"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.list = true
+vim.opt.listchars = { eol = ' ', tab = ' ', trail = '', nbsp = '󱁐' }
+vim.opt.updatetime = 100
+vim.opt.conceallevel = 1
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,12 +4,44 @@ return {
   event = 'VeryLazy',
   main = 'nvim-treesitter.configs',
   opts = {
-    ensure_installed = { 'lua', 'luadoc' },
+    ensure_installed = {
+      'lua',
+      'luadoc',
+      'html',
+      'php',
+      'css',
+      'javascript',
+      'typescript',
+      'python',
+    },
+    autotag = {
+      enable = true,
+    },
     highlight = {
       enable = true,
+      additional_vim_regex_highlighting = true,
     },
     indent = {
       enable = true,
     },
   },
+  config = function ()
+    local parsers = require("nvim-treesitter.parsers").get_parser_configs()
+
+    ---@diagnostic disable-next-line: inject-field
+    parsers.tsx = {
+      filetype_to_parsername = { "javascript", "typescript.tsx" },
+    }
+
+    ---@diagnostic disable-next-line: inject-field
+    parsers.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+        revision = "01e5550cb60ef3532ace0c6df0480f6f406113ff",
+      },
+      filetype = "blade",
+    }
+  end
 }
