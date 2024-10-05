70659480797f19bc10ec7160153d1f4847365d2a
Cristhian Hernandez <cristhianjhl.web@gmail.com>
2024-10-04 20:27:34 -0500

    add gitsigns configurations

--- nvim/after/plugin/gitsign.lua
+++ nvim/after/plugin/gitsign.lua
@@ -13,8 +13,8 @@ gitsigns.setup({
     changedelete = { text = '~' },
     untracked    = { text = '┆' },
   },
-  -- signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-  -- numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
   -- linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
   -- word_diff  = true, -- Toggle with `:Gitsigns toggle_word_diff`
   current_line_blame = true,
@@ -27,8 +27,7 @@ vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
 vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
 vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
 vim.keymap.set('n', 'gt', ':Gitsigns toggle_current_line_blame<CR>')
-vim.keymap.set('n', 'glh', ':Gitsigns toggle_linehl<CR>')
-vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+vim.keymap.set('n', 'gb', ':Gitsigns blame<CR>')
 vim.keymap.set('n', 'gcc', ':G commit -m "')
 vim.keymap.set('n', 'gst', ':G status<CR>')
 vim.keymap.set('n', 'gpm', ':G push origin main<CR>')
