4a9647ad882e306f5a9e27c07d584f8434bf5e9a
cristhianjhl.com <cristhianjhl.web@gmail.com>
2024-12-11 21:37:07 -0500

    update neovim themes

--- nvim/lua/plugins/sweetie.lua
+++ nvim/lua/plugins/sweetie.lua
@@ -1,9 +1,15 @@
 return {
-    "NTBBloodbath/sweetie.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd([[colorscheme sweetie]])
-        vim.cmd([[set background=light]])
-    end,
+  "Carcuis/darcula.nvim",
+  -- "NTBBloodbath/sweetie.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- vim.cmd([[colorscheme sweetie]])
+    -- vim.cmd([[set background=light]])
+
+    vim.cmd([[colorscheme darcula]])
+    vim.cmd([[set background=dark]])
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end,
 }
