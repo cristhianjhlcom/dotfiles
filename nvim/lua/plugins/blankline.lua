3c121e66f4fd6fb31dade658a0415b5be2edd7f6
cristhianjhl.com <cristhianjhl.web@gmail.com>
2024-12-31 00:50:22 -0500

    add blankline plugin settings

---
+++ nvim/lua/plugins/blankline.lua
@@ -0,0 +1,19 @@
+local highlight = {
+  "Whitespace",
+}
+
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = {
+      highlight = highlight,
+      char = "¦"
+    },
+    whitespace = {
+      highlight = highlight,
+      remove_blankline_trail = false,
+    },
+    scope = { enabled = false }
+  },
+}
